5066db0143739274200415e834f6457b840c7cce
Christopher Haster <chaster@utexas.edu>
2022-03-03 19:57:07 -0600

    icecap-qemu: Cleaned up the implementation a bit
    Just some warning/code-level cleanup. More cleanup may be needed in the
    implementation itself.

--- runtime-manager/src/managers/mod.rs
+++ runtime-manager/src/managers/mod.rs
@@ -97,6 +97,13 @@ impl ProtocolState {
         })
     }
 
+    /// Returns the global policy associated with the protocol state.
+    #[inline]
+    #[allow(dead_code)]
+    pub(crate) fn get_policy(&self) -> &Policy {
+        &self.global_policy
+    }
+
     /// Returns the global policy's hash, a hex-encoded string.
     #[inline]
     pub(crate) fn get_policy_hash(&self) -> &str {
@@ -249,7 +256,7 @@ pub fn error_message(message: String, error_code: u32) {
 /// Base function for printing messages outside of the enclave.  Note that this
 /// should only print something to *stdoout* on the host's machine if the debug
 /// configuration flag is set in the Veracruz global policy.
-fn print_message(message: String, code: u32) {
+fn print_message(#[allow(unused)] message: String, #[allow(unused)] code: u32) {
     #[cfg(feature = "linux")]
     if code == 0 {
         eprintln!("Enclave debug message \"{}\"", message);
