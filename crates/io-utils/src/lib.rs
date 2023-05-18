6506f41bfab300c5be6e65ed021b23ade7de2a92
Mathias Brossard <mathias.brossard@arm.com>
2023-05-18 17:52:24 -0500

    Making progress

--- crates/io-utils/src/lib.rs
+++ crates/io-utils/src/lib.rs
@@ -13,14 +13,14 @@
 //! See the `LICENSE.md` file in the Veracruz root directory for
 //! information on licensing and copyright.
 
-#[macro_use]
-extern crate log;
-
 /// IO-related error type.
 pub mod error;
 #[cfg(any(feature = "nitro", feature = "linux", feature = "cca"))]
 /// FD-related material.
 pub mod fd;
+#[cfg(any(feature = "nitro", feature = "linux", feature = "cca"))]
+/// FD-related material.
+pub mod nix;
 #[cfg(feature = "linux")]
 /// TCP-socket related material.
 pub mod tcp;
