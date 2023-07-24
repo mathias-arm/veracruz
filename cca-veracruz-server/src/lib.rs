92a64f3532697a742d5483083ef341a6168f1ea9
Mathias Brossard <mathias.brossard@arm.com>
2023-07-24 11:06:35 -0500

    CCA rebase after server refactoring

--- cca-veracruz-server/src/lib.rs
+++ cca-veracruz-server/src/lib.rs
@@ -1,4 +1,4 @@
-//! Lib target for Icecap Veracruz Server.
+//! Lib target for CCA Veracruz Server.
 //! It's used for unit testing
 //!
 //! ## Authors
@@ -10,15 +10,4 @@
 //! See the `LICENSE_MIT.markdown` file in the Veracruz root directory for
 //! information on licensing and copyright.
 
-<<<<<<< HEAD:icecap-veracruz-server/src/lib.rs
 pub mod server;
-=======
-#[cfg(feature = "cca")]
-pub mod cca;
-#[cfg(feature = "icecap")]
-pub mod icecap;
-#[cfg(feature = "linux")]
-pub mod linux;
-#[cfg(feature = "nitro")]
-pub mod nitro;
->>>>>>> f15aa1b2 (Start support for Linux CCA Realm):veracruz-server/src/platforms/mod.rs
